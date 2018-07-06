ad86351281b9e4d4d1e7e841ce6466fa19183e82
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-07-06 13:31:08 -0600

    json and fetch working

---
+++ app.js
@@ -0,0 +1,37 @@
+console.log("hi from js");
+
+let apiURL = "./waterdata.json";
+
+fetch("./waterdata.json")
+  .then(response => response.json())
+  .then(addElements);
+
+function addElements(data) {
+  console.log(data);
+  var calculatedTotal = document.createElement("h1");
+
+  calculatedTotal.textContent = data[0].action;
+
+  console.log(calculatedTotal);
+
+  document.querySelector("section").appendChild(calculatedTotal);
+}
+
+// function accessAPI() {
+//   return fetch("waterdata.json")
+//   .then(response => console.log(response));
+//   // .then(addElements);
+// }
+
+// function addElements(response) {
+//   console.log(response);
+// }
+// accessAPI();
+
+// function accessAPI() {
+//   const plays = [];
+//   fetch("./package.json")
+//     .then(response => response.json())
+//     .then(data => plays.push(...data));
+// }
+// accessAPI();
