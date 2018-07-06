243f48c096063cf12380bc6bf8cfb3cf6d5199b5
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-07-06 14:16:38 -0600

    app functionality

--- app.js
+++ app.js
@@ -13,25 +13,40 @@ function addElements(data) {
   calculatedTotal.textContent = data[0].action;
 
   console.log(calculatedTotal);
-
-  document.querySelector("section").appendChild(calculatedTotal);
 }
 
-// function accessAPI() {
-//   return fetch("waterdata.json")
-//   .then(response => console.log(response));
-//   // .then(addElements);
-// }
+document
+  .querySelector(".submitButton")
+  .addEventListener("click", getSurveyAnswers);
 
-// function addElements(response) {
-//   console.log(response);
-// }
-// accessAPI();
+function getSurveyAnswers(event) {
+  console.log(event);
 
-// function accessAPI() {
-//   const plays = [];
-//   fetch("./package.json")
-//     .then(response => response.json())
-//     .then(data => plays.push(...data));
-// }
-// accessAPI();
+  document.querySelector("main").removeChild(document.querySelector(".form"));
+
+  var responseSection = document.createElement("section");
+  var laundryFacts = document.createElement("p");
+  var showerFacts = document.createElement("p");
+  var toiletFacts = document.createElement("p");
+  var dishesFacts = document.createElement("p");
+  var hygieneFacts = document.createElement("p");
+
+  laundryFacts.textContent =
+    "The average person uses 42 gallons per week on laundry. You use 60 gallons";
+  showerFacts.textContent =
+    "The average person uses 140 gallons per week in the shower. You use 210 gallons";
+  toiletFacts.textContent =
+    "The average person uses 63.7 gallons per week flushing the toilet. You use 67.2 gallons";
+  hygieneFacts.textContent =
+    "The average person uses 19.25 gallons per week on personal hygiene. You use 21 gallons.";
+  dishesFacts.textContent =
+    "The average person uses 70 gallons per week handwashing dishes. You use the same! You can save water by using a dishwasher!";
+
+  responseSection.appendChild(laundryFacts);
+  responseSection.appendChild(showerFacts);
+  responseSection.appendChild(toiletFacts);
+  responseSection.appendChild(dishesFacts);
+  responseSection.appendChild(hygieneFacts);
+
+  document.querySelector("main").appendChild(responseSection);
+}
